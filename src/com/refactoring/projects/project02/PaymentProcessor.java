e795183103bd05ddf7e5ed50907b6e340e6fcecf
DaniGomezDelgado <Danielgomezdelga@gmail.com>
2026-02-26 19:35:15 +0100

    Cambiar los valores constantes por constantes en el final

--- src/com/refactoring/projects/project02/PaymentProcessor.java
+++ src/com/refactoring/projects/project02/PaymentProcessor.java
@@ -2,17 +2,23 @@ package com.refactoring.projects.project02;
 
 public class PaymentProcessor {
 
+    final double COMISION = 1.05;
+    final double DESCUENTO = 0.9;
+    final double DESCUENTO_POR_VOLUMEN = 0.95;
+    
 	// AÑADIR ENUM
    public double calcularTotal(double precio, String tipoPago, boolean esCliente, int cantidad) {
         double total = precio * cantidad;
+        
+        
         if (tipoPago.equals("tarjeta")) {
-            total = total * 1.05; // 5% comisión
+            total = total * COMISION; // 5% comisión
         }
         if (esCliente) {
-            total = total * 0.9; // 10% descuento
+            total = total * DESCUENTO; // 10% descuento
         }
         if (cantidad > 10) {
-            total = total * 0.95; // 5% descuento por volumen
+            total = total * DESCUENTO_POR_VOLUMEN; // 5% descuento por volumen
         }
         return total;
     }
